ae22d6374ae6b6941ffa22afb435799baa0cf428
luke-goldfain <lukegoldfain@gmail.com>
2018-04-13 12:02:15 -0500

    pickup testing scene, particles in playertesting scene

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class Character : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField]
+    GameState GameState;
+    [SerializeField]
+    List<GameObject> Players;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+        switch(GameState)
+        {
+
+            case GameState.STARTING:
+                {
+
+                    Starting();
+                    break;
+
+                }
+            case GameState.PLAYING:
+                {
+
+                    Playing();
+                    break;
+
+                }
+            case GameState.ENDING:
+                {
+
+                    Ending();
+                    break;
+
+                }
+
+        }
+
+	}
+
+    private void Starting()
+    {
+
+
+    }
+
+    private void Playing()
+    {
+
+
+    }
+
+    private void Ending()
+    {
+
+
+
+    }
+
+}
